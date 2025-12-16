eaaacceb69e3614d269b60cb944cca66b5a59e4c
paulAlexSerban <paul.alex.serban@gmail.com>
2025-12-16 15:18:09 +0200

    feat(arch): cleanup

--- assets/media/scripts/publish.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-# makes sure the folder containing the script will be the root folder
-cd "$(dirname "$0")" || exit
-
-source ../../../.env
-
-echo "🔧  Publisjing assets to AWS S3"
-node ../aws/push-assets.js
